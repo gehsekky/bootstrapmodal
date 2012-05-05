1899e754c12b8bb3edccb422b2803e4e62fc9918
andy chung <chung.andy@gmail.com>
2012-05-05 18:21:09 -0400

    added public hide method. added ability to edit close button text.

--- jquery.bootstrapmodal.js
+++ jquery.bootstrapmodal.js
@@ -4,37 +4,38 @@
 		message: '',
 		primaryButtonText: '',
 		primaryButtonClick: null,
+		closeButtonText: 'Close',
 		disableFooter: false,
 		keyboard: true,
 		backdrop: 'static'
 	};
-	
+
 	var options;
-	
+
 	$.fn.bootstrapmodal = function (action, runtimeOptions) {
 		if (typeof (action) === 'object') {
 			runtimeOptions = action;
 			action = 'initialize';
 		}
-		
+
 		options = $.extend({}, defaults, runtimeOptions);
-		
+
 		this.each(function () {
 			var $this = $(this);
 			switch (action) {
 				case 'initialize':
 					// dialog html
-					var html = 	'<div class="modal-header">' + 
-									'<a class="close" data-dismiss="modal">×</a>' + 
-									'<h3 class="bootstrapmodal-title"></h3>' + 
-								'</div>' + 
-								'<div class="modal-body">' + 
-								'</div>' + 
-								'<div class="modal-footer">' + 
-									'<a href="javascript:void(0);" class="btn bootstrapmodal-button-close">Close</a>' + 
-									'<a href="javascript:void(0);" class="btn btn-primary bootstrapmodal-button-primary"></a>' + 
+					var html = '<div class="modal-header">' +
+									'<a class="close" data-dismiss="modal">×</a>' +
+									'<h3 class="bootstrapmodal-title"></h3>' +
+								'</div>' +
+								'<div class="modal-body">' +
+								'</div>' +
+								'<div class="modal-footer">' +
+									'<a href="javascript:void(0);" class="btn bootstrapmodal-button-close"></a>' +
+									'<a href="javascript:void(0);" class="btn btn-primary bootstrapmodal-button-primary"></a>' +
 								'</div>';
-					
+
 					// initialize modal container
 					$this
 						.addClass('modal')
@@ -42,10 +43,10 @@
 
 					// set modal title
 					$('.bootstrapmodal-title', $this).html(options.title);
-					
+
 					// set message
 					$('.modal-body', $this).html(options.message);
-					
+
 					// initialize primary button. hide if no text.
 					if (options.primaryButtonText === '') {
 						$('.bootstrapmodal-button-primary', $this).css('display', 'none');
@@ -53,25 +54,29 @@
 						$('.bootstrapmodal-button-primary', $this)
 							.css('display', '')
 							.html(options.primaryButtonText);
-						
+
 						// bind click
 						if (typeof (options.primaryButtonClick) === 'function') {
 							$('.bootstrapmodal-button-primary', $this).on('click', options.primaryButtonClick);
 						}
 					}
-					
+
+					// Set text for close button
+					$('.bootstrapmodal-button-close', $this)
+							.html(options.closeButtonText);
+
 					// bind click for close button
 					$('.bootstrapmodal-button-close', $this).on('click', function () {
 						$this.modal('hide');
 					});
-					
+
 					// set footer visibility
 					if (options.disableFooter) {
 						$('.modal-footer', $this).css('display', 'none');
 					} else {
 						$('.modal-footer', $this).css('display', '');
 					}
-					
+
 					// init and show modal
 					$this.modal({
 						backdrop: options.backdrop,
@@ -83,11 +88,13 @@
 					$this.empty();
 					$this.css('display', 'none');
 					break;
+				case 'hide':
+					$this.modal('hide');
 				default:
 					break;
 			}
 		});
-		
+
 		return this;
 	};
 })(jQuery);
